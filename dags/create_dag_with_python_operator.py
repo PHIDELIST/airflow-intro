92a79bfb6f36b5fb4b77f7bde8916ffd5e64f1bd
PHIDELIST <phidelisoluoch@gmail.com>
2024-01-16 19:09:05 +0300

    dag with parametors

--- dags/create_dag_with_python_operator.py
+++ dags/create_dag_with_python_operator.py
@@ -8,18 +8,20 @@ default_args = {
     'retry_delay':timedelta(minutes=5)
 }
 
-def greet():
-    print("Hello World!")
+def greet(name,age):
+    print(f"Hello World! my name is {name}"
+          f" and am {age} years old")
 
 
 with DAG(
     default_args=default_args,
-    dag_id='v1_python_operator',
+    dag_id='v2_python_operator',
     description='dag using python operator',
     start_date=datetime(2024,10,6),
     schedule_interval='@daily'
 ) as dag:
       task1 = PythonOperator(
         task_id='greet',
-        python_callable=greet
+        python_callable=greet,
+        op_kwargs={'name':'phidel','age':23}
     )
